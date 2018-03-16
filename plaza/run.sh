5d20445ca008163c61485017e3c9cb7d592cc80d
Patrice LACHANCE <patlachance@gmail.com>
2018-03-16 09:51:44 +0100

    debuging

--- plaza/run.sh
+++ plaza/run.sh
@@ -8,6 +8,14 @@ _log() {
 
 _log "init script starting"
 
+_log "fixing /etc/passwd"
+if ! whoami &> /dev/null; then
+  if [ -w /etc/passwd ]; then
+  echo "${USER_NAME:-default}:x:$(id -u):0:${USER_NAME:-default}
+  user:${HOME}:/sbin/nologin" >> /etc/passwd
+  fi
+fi
+
 _log "copying go dependencies install script on shared volume"
 cp $SRCDIR/install.sh /go/src/github.com/Nanocloud/nanocloud/plaza/install.sh
 
